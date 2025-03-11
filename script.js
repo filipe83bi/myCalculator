e4f7d7e2019f474cd69fe3fce117ebac0f76fc6e
filipe83bi <kerubimmusik@gmail.com>
2025-03-11 14:38:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const firstNumberInput = document.getElementById("first-number");
+const secondNumberInput = document.getElementById("second-number");
+const operatorInput = document.getElementById("operator");
+const button = document.getElementById("button");
+const resultInputArea = document.getElementById("result");
+
+const calculate = function (firstNumber, secondNumber) {
+  let result;
+
+  firstNumber = Number(firstNumberInput.value);
+  secondNumber = Number(secondNumberInput.value);
+  const operator = operatorInput.value;
+
+  if (operator === "+") result = firstNumber + secondNumber;
+  if (operator === "-") result = firstNumber - secondNumber;
+  if (operator === "*") result = firstNumber * secondNumber;
+  if (operator === "/") result = firstNumber / secondNumber;
+
+  resultInputArea.innerText = "Result: " + result;
+};
+
+button.addEventListener("click", calculate);
