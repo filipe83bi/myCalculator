eac8fd1b573ce1129a0596ffe23d48165dbe890a
filipe83bi <kerubimmusik@gmail.com>
2025-03-13 10:24:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ const operatorElement = document.getElementById("operator");
 const button = document.getElementById("button");
 const resultElementArea = document.getElementById("result");
 
+firstNumberElement.addEventListener("keydown", (e) => {
+  if (e.key === "0") {
+    resultElementArea.innerText = `0 can not be the first number.`;
+    return;
+  }
+});
+
 const calculate = function (firstNumber, secondNumber) {
   let result;
 
@@ -15,11 +22,13 @@ const calculate = function (firstNumber, secondNumber) {
 
   if (isNaN(firstNumber) || isNaN(secondNumber)) {
     resultElementArea.innerText = `Please enter a valid number`;
+    firstNumberElement.value = "";
     return;
   }
-
   if (firstNumber === 0) {
-    resultElementArea.innerText = "The first number cannot be 0";
+    resultElementArea.innerText = `Please enter a valid number`;
+    firstNumberElement.value = "";
+    secondNumberElement.value = "";
     return;
   }
 
@@ -59,6 +68,14 @@ const calculate = function (firstNumber, secondNumber) {
   //   }
   // }
   resultElementArea.innerText = `Result: ${result}`;
+  firstNumberElement.value = "";
+  secondNumberElement.value = "";
 };
 
 button.addEventListener("click", calculate);
+
+document.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    calculate();
+  }
+});
